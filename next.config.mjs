ab0c0381190d4c06d94257d48a191104c1230976
Gogo200202 <gosheto420@gmail.com>
2024-06-07 15:49:52 +0300

    fix again

--- next.config.mjs
+++ next.config.mjs
@@ -2,13 +2,9 @@
 * @type {import('next').NextConfig}
 */
 const nextConfig = {
-    output: "export",
-    images: {
-      loader: "akamai",
-      path: "",
-    },
-    base:"/test-app"
-   // assetPrefix: "./",
+   
+    base:"/test-app",
+   //assetPrefix: "./",
   };
   
   export default nextConfig;
